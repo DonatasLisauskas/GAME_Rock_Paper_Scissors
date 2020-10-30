0bcf83e28761e3e1892e28a12d5795b451bfa318
Donatas Lisauskas <donliskas@gmail.com>
2020-10-30 13:28:38 +0200

    The project of "Rock - Paper - Scissors" are created;

---
+++ src/main/java/ItemsAbstract.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ItemsAbstract {
+
+    protected final List<String> itemList = new ArrayList<String>();
+
+    protected ItemsAbstract() {
+        itemList.add("ROCK");
+        itemList.add("PAPER");
+        itemList.add("SCISSORS");
+    }
+}
