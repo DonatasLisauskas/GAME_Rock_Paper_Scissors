86adb0473856ebb2eabee7f17c3ee82fbf63e035
Donatas Lisauskas <donliskas@gmail.com>
2020-11-09 10:56:18 +0200

    the commented code was removed.

--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -6,9 +6,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Play {
     public static void main(String[] args) {
-     /*   PlayGame newGame = new Game();
-        newGame.playGame();*/
-
         ApplicationContext context = new AnnotationConfigApplicationContext(ConfigGame.class);
         PlayGame game = context.getBean(Game.class);
         game.playGame();
