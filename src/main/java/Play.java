94b53fdf5cfab4915de66b958ea9c65450ac78bd
Donatas Lisauskas <donliskas@gmail.com>
2020-11-09 23:49:54 +0200

    The Spring framework is integrated to the project.

--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -7,19 +7,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Play {
     public static void main(String[] args) {
-      /*  ApplicationContext context = new AnnotationConfigApplicationContext(GameConfiguration.class);
-        PlayGame game = context.getBean(Game.class);
-        game.playGame();*/
-
-      /*  GameActions playGame = new Game( new FirstPlayer(), new ComputerPlayer());
-        playGame.playGame();*/
-
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-
         GameActions game = context.getBean("gameActions", Game.class);
-
         game.playGame();
-
-
     }
 }
